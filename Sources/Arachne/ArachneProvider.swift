0dccaa5df3139c0b5ed7d74be49c87a40199b078
Cristina De Rito <artemisia-absynthium@users.noreply.github.com>
2021-10-18 18:04:08 +0200

    Allow to extend provider methods

--- Sources/Arachne/ArachneProvider.swift
+++ Sources/Arachne/ArachneProvider.swift
@@ -38,7 +38,7 @@ open class ArachneProvider<T: ArachneService> {
     ///   - timeoutInterval: Optional timeout interval in seconds. Default value is the default of `URLRequest`: 60 seconds.
     ///   - session: Optionally pass any session you want to use instead of the default `URLSession.default`.
     /// - Returns: A publisher publishing a value of type `responseType` or an `Error` if anything goes wrong in the pipeline.
-    public func request<ResponseType: Decodable>(_ target: T,
+    open func request<ResponseType: Decodable>(_ target: T,
                                           responseType: ResponseType.Type,
                                           decoder: JSONDecoder = JSONDecoder(),
                                           timeoutInterval: Double? = nil,
@@ -83,7 +83,7 @@ open class ArachneProvider<T: ArachneService> {
     ///   - timeoutInterval: Optional timeout interval in seconds. Default value is the default of `URLRequest`: 60 seconds.
     ///   - session: Optionally pass any session you want to use instead of the default `URLSession.default`.
     /// - Returns: A publisher publishing a tuple containing the temporary URL of the downloaded file and the `URLResponse` or an `Error` if anything goes wrong in the pipeline.
-    public func download(_ target: T, fileName: String, timeoutInterval: Double? = nil,
+    open func download(_ target: T, fileName: String, timeoutInterval: Double? = nil,
                   session: URLSession? = nil) -> AnyPublisher<(URL, URLResponse), Error> {
         let request: URLRequest
         do {
