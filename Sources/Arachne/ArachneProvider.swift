08137a4d6e62e17d22fee9bfe698bb18efd0a74d
Cristina De Rito <artemisia-absynthium@users.noreply.github.com>
2022-02-26 23:19:27 +0100

    Fix issue reported by Codacy

--- Sources/Arachne/ArachneProvider.swift
+++ Sources/Arachne/ArachneProvider.swift
@@ -126,7 +126,8 @@ open class ArachneProvider<T: ArachneService> {
     /// Builds a `URLRequest` from an `ArachneService` endpoint definition.
     /// - Parameters:
     ///   - target: An endpoint.
-    ///   - timeoutInterval: Optional timeout interval in seconds. Default value is the default of `URLRequest`: 60 seconds.
+    ///   - timeoutInterval: Optional timeout interval in seconds.
+    ///   Default value is the default of `URLRequest`: 60 seconds.
     /// - Returns: The built `URLRequest`.
     public func buildRequest(target: T, timeoutInterval: Double?) throws -> URLRequest {
         let url = try URLUtil.composedUrl(for: target)
