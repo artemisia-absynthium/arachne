3f8a1471872505a210c34e06a49532c4f9f32f63
Cristina De Rito <artemisia-absynthium@users.noreply.github.com>
2023-01-27 22:01:14 +0100

    Remove support to Combine

--- Sources/Arachne/ArachneProvider.swift
+++ Sources/Arachne/ArachneProvider.swift
@@ -1,35 +1,20 @@
 //
-//  ArachneProvider.swift
-//  
+// ArachneProvider.swift - the provider implementation
+// This source file is part of the Arachne open source project
 //
-//  Created by Cristina De Rito on 30/09/21.
+// Copyright (c) 2021 - 2023 artemisia-absynthium
+// Licensed under MIT
+//
+// See https://github.com/artemisia-absynthium/arachne/blob/main/LICENSE for license information
 //
 
 import Foundation
-import Combine
 
 /// Use ``ArachneProvider`` to make requests to a specific ``ArachneService``.
 open class ArachneProvider<T: ArachneService> {
     private let urlSession: URLSession
     private let plugins: [ArachnePlugin]?
-    private let signingPublisher: ((T, URLRequest) -> AnyPublisher<URLRequest, URLError>)?
     private let signingFunction: ((T, URLRequest) async throws -> URLRequest)?
-
-    /// Initialize a provider that uses a Combine Publisher to sign requests.
-    /// - Parameters:
-    ///   - urlSession: Your `URLSession`, uses default if none is passed.
-    ///   - plugins: An optional array of ``ArachnePlugin``s.
-    ///   - signingPublisher: An optional Combine Publisher `(T, URLRequest) -> AnyPublisher<URLRequest, URLError>`
-    ///   publisher that outputs the request received as input signed.
-    @available(*, deprecated, message: "Use init(urlSession:signingFunction:plugins:) instead")
-    public init(urlSession: URLSession = URLSession(configuration: .default),
-                plugins: [ArachnePlugin]? = nil,
-                signingPublisher: ((T, URLRequest) -> AnyPublisher<URLRequest, URLError>)? = nil) {
-        self.urlSession = urlSession
-        self.plugins = plugins
-        self.signingPublisher = signingPublisher
-        self.signingFunction = nil
-    }
 
     /// Initialize a provider that uses an asynchronous function to sign requests.
     /// - Parameters:
@@ -37,83 +22,11 @@ open class ArachneProvider<T: ArachneService> {
     ///   - plugins: An optional array of ``ArachnePlugin``s.
     ///   - signingFunction: An optional async throwing function that signs the request before it is made.
     public init(urlSession: URLSession = URLSession(configuration: .default),
-                signingFunction: ((T, URLRequest) async throws -> URLRequest)?,
-                plugins: [ArachnePlugin]?) {
+                signingFunction: ((T, URLRequest) async throws -> URLRequest)? = nil,
+                plugins: [ArachnePlugin]? = nil) {
         self.urlSession = urlSession
         self.plugins = plugins
-        self.signingPublisher = nil
         self.signingFunction = signingFunction
-    }
-
-    // MARK: - Combine
-
-    /// Make a request to an endpoint defined in an ``ArachneService``.
-    /// - Parameters:
-    ///   - target: An endpoint.
-    ///   - timeoutInterval: Optional timeout interval in seconds.
-    ///   Default value is the default of `URLRequest`: 60 seconds.
-    ///   - session: Optionally pass any session you want to use instead of the one of the provider.
-    /// - Returns: A publisher publishing a value of type `Data`or an `Error` if anything goes wrong in the pipeline.
-    @available(*, deprecated, message: "Use data(_:timeoutInterval:session:) instead")
-    open func request(_ target: T,
-                      timeoutInterval: Double? = nil,
-                      session: URLSession? = nil) -> AnyPublisher<Data, Error> {
-        let request: URLRequest
-        do {
-            request = try buildRequest(target: target, timeoutInterval: timeoutInterval)
-        } catch {
-            return Fail(error: error)
-                .eraseToAnyPublisher()
-        }
-        return buildSigningPublisher(target: target, request: request)
-            .flatMap { request -> URLSession.DataTaskPublisher in
-                self.plugins?.forEach { $0.handle(request: request) }
-                return (session ?? self.urlSession).dataTaskPublisher(for: request)
-            }
-            .tryMap { data, response in
-                return try self.handleDataResponse(target: target, data: data, response: response)
-            }
-            .mapError { error in
-                return self.handleAndReturn(error: error, request: request)
-            }
-            .receive(on: DispatchQueue.main)
-            .eraseToAnyPublisher()
-    }
-
-    /// Download a resource from an endpoint defined in an ``ArachneService``.
-    ///
-    /// The downloaded file must be copied in the appropriate folder to be used, because Arachne makes no assumption
-    /// on whether it must be cached or not so it just returns the same URL returned from `URLSession.downloadTask`.
-    /// - Parameters:
-    ///   - target: An endpoint.
-    ///   - timeoutInterval: Optional timeout interval in seconds.
-    ///   Default value is the default of `URLRequest`: 60 seconds.
-    ///   - session: Optionally pass any session you want to use instead of the one of the provider.
-    /// - Returns: A publisher publishing a tuple containing the temporary URL of the downloaded file
-    /// and the `URLResponse` or an `Error` if anything goes wrong in the pipeline.
-    @available(*, deprecated, message: "Use download(_:timeoutInterval:session:) instead")
-    open func download(_ target: T,
-                       timeoutInterval: Double? = nil,
-                       session: URLSession? = nil) -> AnyPublisher<(URL, URLResponse), Error> {
-        let request: URLRequest
-        do {
-            request = try buildRequest(target: target, timeoutInterval: timeoutInterval)
-        } catch {
-            return Fail(error: error)
-                .eraseToAnyPublisher()
-        }
-        return buildSigningPublisher(target: target, request: request)
-            .flatMap { request -> URLSession.DownloadTaskPublisher in
-                self.plugins?.forEach { $0.handle(request: request) }
-                return (session ?? self.urlSession).downloadTaskPublisher(for: request)
-            }
-            .tryMap { url, response in
-                return try self.handleDownloadResponse(target: target, url: url, response: response)
-            }
-            .mapError { error in
-                return self.handleAndReturn(error: error, request: request)
-            }
-            .eraseToAnyPublisher()
     }
 
     // MARK: - Async/Await
@@ -208,21 +121,6 @@ open class ArachneProvider<T: ArachneService> {
         var signedRequest = request
         if let signingFunction = signingFunction {
             signedRequest = try await signingFunction(target, request)
-        } else if let signingPublisher = signingPublisher {
-            var cancellable: AnyCancellable?
-            signedRequest = try await withCheckedThrowingContinuation { continuation in
-                cancellable = signingPublisher(target, request)
-                    .sink(receiveCompletion: { completion in
-                        switch completion {
-                        case .finished:
-                            break
-                        case .failure(let error):
-                            continuation.resume(throwing: error)
-                        }
-                    }, receiveValue: { request in
-                        continuation.resume(returning: request)
-                    })
-            }
         }
         return signedRequest
     }
@@ -258,33 +156,6 @@ open class ArachneProvider<T: ArachneService> {
         let output = self.extractOutput(from: error)
         self.plugins?.forEach { $0.handle(error: error, request: request, output: output) }
         return error
-    }
-
-    private func buildSigningPublisher(target: T, request: URLRequest) -> AnyPublisher<URLRequest, URLError> {
-        let pub: AnyPublisher<URLRequest, URLError>
-        if let signingPub = signingPublisher {
-            pub = signingPub(target, request)
-                .eraseToAnyPublisher()
-        } else if let signingFunction = signingFunction {
-            pub = Future { promise in
-                Task {
-                    do {
-                        let signedRequest = try await signingFunction(target, request)
-                        promise(.success(signedRequest))
-                    } catch let error as URLError {
-                        promise(.failure(error))
-                    } catch {
-                        promise(.failure(URLError(.userAuthenticationRequired, userInfo: [NSUnderlyingErrorKey: error])))
-                    }
-                }
-            }
-            .eraseToAnyPublisher()
-        } else {
-            pub = Just(request)
-                .setFailureType(to: URLError.self)
-                .eraseToAnyPublisher()
-        }
-        return pub
     }
 
     private func extractOutput(from error: Error) -> Any? {
--- Tests/ArachneTests/ArachneTests.swift
+++ Tests/ArachneTests/ArachneTests.swift
@@ -1,38 +1,20 @@
+//
+// ArachneTests.swift - test suite
+// This source file is part of the Arachne open source project
+//
+// Copyright (c) 2021 - 2023 artemisia-absynthium
+// Licensed under MIT
+//
+// See https://github.com/artemisia-absynthium/arachne/blob/main/LICENSE for license information
+//
+
 import XCTest
-import Combine
 @testable import Arachne
 
 final class ArachneTests: XCTestCase {
     let timeout: TimeInterval = 10
-    var cancellable: AnyCancellable?
-
-    override func tearDown() {
-        super.tearDown()
-        cancellable = nil
-    }
 
     func testGet() throws {
-        let expectation = XCTestExpectation(description: "Download my Github user info")
-
-        let provider = ArachneProvider<Github>()
-        cancellable = provider.request(.userProfile("artemisia-absynthium"))
-            .decode(type: GithubUser.self, decoder: JSONDecoder())
-            .sink { completion in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    XCTFail("Unexpected error: \(error.localizedDescription)")
-                }
-            } receiveValue: { user in
-                XCTAssertEqual(user, GithubUser(login: "artemisia-absynthium"))
-                expectation.fulfill()
-            }
-
-        wait(for: [expectation], timeout: timeout)
-    }
-
-    func testAsyncGet() throws {
         let expectation = XCTestExpectation(description: "Download my Github user info")
 
         let provider = ArachneProvider<Github>()
@@ -54,30 +36,6 @@ final class ArachneTests: XCTestCase {
         let expectation = XCTestExpectation(description: "Service URL is malformed")
 
         let provider = ArachneProvider<Dummy>()
-        cancellable = provider.request(.malformedUrl)
-            .sink(receiveCompletion: { completion in
-                switch completion {
-                case .failure(let error):
-                    let castedError = error as? ARError
-                    let expectedError = ARError.malformedUrl("htp:🥶/malformedUrl")
-                    XCTAssertNotNil(castedError)
-                    XCTAssertEqual(castedError?.errorCode, expectedError.errorCode)
-                    XCTAssertEqual(castedError?.localizedDescription, expectedError.localizedDescription)
-                    expectation.fulfill()
-                case .finished:
-                    XCTFail("Request shouldn't have finished, URL should be malformed")
-                }
-            }, receiveValue: { _ in
-                XCTFail("Shouldn't receive any value, URL is malformed")
-            })
-
-        wait(for: [expectation], timeout: timeout)
-    }
-
-    func testAsyncMalformedUrlErrorBecauseOfBaseUrl() throws {
-        let expectation = XCTestExpectation(description: "Service URL is malformed")
-
-        let provider = ArachneProvider<Dummy>()
         Task {
             do {
                 _ = try await provider.data(.malformedUrl)
@@ -96,34 +54,6 @@ final class ArachneTests: XCTestCase {
     }
 
     func testMalformedUrlErrorBecauseOfPath() throws {
-        let expectation = XCTestExpectation(description: "Endpoint path is malformed")
-
-        let provider = ArachneProvider<Dummy>()
-        cancellable = provider.request(.nilUrl)
-            .sink(receiveCompletion: { completion in
-                switch completion {
-                case .failure(let error):
-                    let castedError = error as? ARError
-                    var urlComponents = URLComponents(string: "https://malformedquerystring.io")
-                    let endpoint = Dummy.nilUrl
-                    urlComponents?.path = endpoint.path
-                    urlComponents?.queryItems = endpoint.queryStringItems
-                    let expectedError = ARError.malformedUrl(urlComponents?.description ?? "")
-                    XCTAssertNotNil(castedError)
-                    XCTAssertEqual(castedError?.errorCode, expectedError.errorCode)
-                    XCTAssertEqual(castedError?.localizedDescription, expectedError.localizedDescription)
-                    expectation.fulfill()
-                case .finished:
-                    XCTFail("Request shouldn't have finished, URL should be malformed")
-                }
-            }, receiveValue: { _ in
-                XCTFail("Shouldn't receive any value, URL is malformed")
-            })
-
-        wait(for: [expectation], timeout: timeout)
-    }
-
-    func testAsyncMalformedUrlErrorBecauseOfPath() throws {
         let expectation = XCTestExpectation(description: "Endpoint path is malformed")
 
         let provider = ArachneProvider<Dummy>()
@@ -178,32 +108,6 @@ final class ArachneTests: XCTestCase {
         let expectation = XCTestExpectation(description: "Download an image")
 
         let provider = ArachneProvider<Github>()
-        cancellable = provider.download(.avatar(id))
-            .sink { completion in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    XCTFail("Unexpected error: \(error.localizedDescription)")
-                }
-            } receiveValue: { url, response in
-                let fileExists = FileManager.default.fileExists(atPath: url.path)
-                XCTAssertTrue(fileExists, "Downloaded file doesn't exist")
-                let httpResponse = response as? HTTPURLResponse
-                XCTAssertNotNil(httpResponse)
-                XCTAssertEqual(httpResponse?.statusCode, 200)
-                expectation.fulfill()
-            }
-
-        wait(for: [expectation], timeout: 20)
-    }
-
-    func testAsyncDownload() throws {
-        let id = "7290872"
-
-        let expectation = XCTestExpectation(description: "Download an image")
-
-        let provider = ArachneProvider<Github>()
         Task {
             do {
                 let (url, response) = try await provider.download(.avatar(id))
@@ -222,42 +126,6 @@ final class ArachneTests: XCTestCase {
     }
 
     func testRequestUnacceptableStatusCodeError() throws {
-        let expectation = XCTestExpectation(description: "Request returns an unacceptable status code")
-
-        let provider = ArachneProvider<Github>()
-        cancellable = provider.request(.notFound)
-            .sink(receiveCompletion: { completion in
-                switch completion {
-                case .failure(let error):
-                    let castedError = error as? ARError
-                    switch castedError {
-                    case .malformedUrl:
-                        XCTFail("Error shouldn't be malformed URL")
-                    case .unacceptableStatusCode(let statusCode, let response, let data):
-                        let expectedError = ARError.unacceptableStatusCode(statusCode: 404,
-                                                                           response: HTTPURLResponse(),
-                                                                           responseContent: Data())
-                        XCTAssertNotNil(castedError)
-                        XCTAssertEqual(castedError?.errorCode, expectedError.errorCode)
-                        XCTAssertEqual(statusCode, 404)
-                        XCTAssertNotNil(response)
-                        XCTAssertNotNil(data)
-                        XCTAssertEqual(castedError?.localizedDescription, expectedError.localizedDescription)
-                        expectation.fulfill()
-                    case .none:
-                        XCTFail("Error shouldn't be none")
-                    }
-                case .finished:
-                    XCTFail("Request shouldn't have finished, status code should be unacceptable")
-                }
-            }, receiveValue: { _ in
-                XCTFail("Shouldn't receive any value, status code should be unacceptable")
-            })
-
-        wait(for: [expectation], timeout: timeout)
-    }
-
-    func testAsyncRequestUnacceptableStatusCodeError() throws {
         let expectation = XCTestExpectation(description: "Request returns an unacceptable status code")
 
         let provider = ArachneProvider<Github>()
@@ -289,42 +157,6 @@ final class ArachneTests: XCTestCase {
     }
 
     func testDownloadUnacceptableStatusCodeError() throws {
-        let expectation = XCTestExpectation(description: "Request returns an unacceptable status code")
-
-        let provider = ArachneProvider<Github>()
-        cancellable = provider.download(.notFound)
-            .sink(receiveCompletion: { completion in
-                switch completion {
-                case .failure(let error):
-                    let castedError = error as? ARError
-                    switch castedError {
-                    case .malformedUrl:
-                        XCTFail("Error shouldn't be malformed URL")
-                    case .unacceptableStatusCode(let statusCode, let response, let data):
-                        let expectedError = ARError.unacceptableStatusCode(statusCode: 404,
-                                                                           response: HTTPURLResponse(),
-                                                                           responseContent: Data())
-                        XCTAssertNotNil(castedError)
-                        XCTAssertEqual(castedError?.errorCode, expectedError.errorCode)
-                        XCTAssertEqual(statusCode, 404)
-                        XCTAssertNotNil(response)
-                        XCTAssertNotNil(data)
-                        XCTAssertEqual(castedError?.localizedDescription, expectedError.localizedDescription)
-                        expectation.fulfill()
-                    case .none:
-                        XCTFail("Error shouldn't be none")
-                    }
-                case .finished:
-                    XCTFail("Request shouldn't have finished, status code should be unacceptable")
-                }
-            }, receiveValue: { _ in
-                XCTFail("Shouldn't receive any value, status code should be unacceptable")
-            })
-
-        wait(for: [expectation], timeout: timeout)
-    }
-
-    func testAsyncDownloadUnacceptableStatusCodeError() throws {
         let expectation = XCTestExpectation(description: "Request returns an unacceptable status code")
 
         let provider = ArachneProvider<Github>()
@@ -392,51 +224,6 @@ final class ArachneTests: XCTestCase {
         let plugin = TestPlugin(errorExpectation: errorExpectation, requestExpectation: requestExpectation)
 
         let provider = ArachneProvider<Github>(plugins: [plugin])
-        cancellable = provider.request(.notFound)
-            .sink(receiveCompletion: { _ in }, receiveValue: { _ in
-                XCTFail("Shouldn't receive any value, status code should be unacceptable")
-            })
-
-        wait(for: [errorExpectation, requestExpectation], timeout: timeout)
-    }
-
-    func testAsyncPluginHasErrorResponse() throws {
-        struct TestPlugin: ArachnePlugin {
-            let errorExpectation: XCTestExpectation
-            let requestExpectation: XCTestExpectation
-
-            func handle(error: Error, request: URLRequest, output: Any?) {
-                XCTAssertEqual(request.url?.absoluteString,
-                               "https://api.github.com/notFound",
-                               "Request URL in error is not equal to expected URL")
-                XCTAssertTrue(error is ARError)
-                switch error as? ARError {
-                case .some(.unacceptableStatusCode(let statusCode, _, _)):
-                    XCTAssertEqual(statusCode, 404)
-                case .none:
-                    XCTFail("Error is none but should be unacceptableStatusCode")
-                case .some(.malformedUrl(_)):
-                    XCTFail("Error is malformedUrl but should be unacceptableStatusCode")
-                }
-                XCTAssertNotNil(output)
-                errorExpectation.fulfill()
-            }
-
-            func handle(request: URLRequest) {
-                XCTAssertEqual(request.url?.absoluteString, "\(Github.notFound.baseUrl)\(Github.notFound.path)")
-                requestExpectation.fulfill()
-            }
-
-            func handle(response: URLResponse, data: Any) {
-                XCTFail("Should not have been called")
-            }
-        }
-
-        let errorExpectation = XCTestExpectation(description: "The plugin has correct error response data")
-        let requestExpectation = XCTestExpectation(description: "The plugin handles the correct request")
-        let plugin = TestPlugin(errorExpectation: errorExpectation, requestExpectation: requestExpectation)
-
-        let provider = ArachneProvider<Github>(plugins: [plugin])
         Task {
             do {
                 _ = try await provider.data(.notFound)
@@ -449,95 +236,7 @@ final class ArachneTests: XCTestCase {
         wait(for: [errorExpectation, requestExpectation], timeout: timeout)
     }
 
-    func testSigningPublisherWithCombineRequest() throws {
-        let signingPublisher: (Github, URLRequest) -> AnyPublisher<URLRequest, URLError> = { _, request in
-            var mutableRequest = request
-            var url = request.url?.absoluteString ?? ""
-            url += "artemisia-absynthium"
-            mutableRequest.url = URL(string: url)
-            return Just(mutableRequest)
-                .setFailureType(to: URLError.self)
-                .eraseToAnyPublisher()
-        }
-        let expectation = XCTestExpectation(
-            description: "Request is modified by the signingPublisher and returns a valid user")
-
-        let provider = ArachneProvider<Github>(signingPublisher: signingPublisher)
-        cancellable = provider.request(.userProfile(""))
-            .decode(type: GithubUser.self, decoder: JSONDecoder())
-            .sink { completion in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    XCTFail("Unexpected error: \(error.localizedDescription)")
-                }
-            } receiveValue: { user in
-                XCTAssertEqual(user, GithubUser(login: "artemisia-absynthium"))
-                expectation.fulfill()
-            }
-
-        wait(for: [expectation], timeout: timeout)
-    }
-
-    func testSigningPublisherWithAsyncRequest() throws {
-        let signingPublisher: (Github, URLRequest) -> AnyPublisher<URLRequest, URLError> = { _, request in
-            var mutableRequest = request
-            var url = request.url?.absoluteString ?? ""
-            url += "artemisia-absynthium"
-            mutableRequest.url = URL(string: url)
-            return Just(mutableRequest)
-                .setFailureType(to: URLError.self)
-                .eraseToAnyPublisher()
-        }
-        let expectation = XCTestExpectation(
-            description: "Request is modified by the signingPublisher and returns a valid user")
-
-        let provider = ArachneProvider<Github>(signingPublisher: signingPublisher)
-        Task {
-            do {
-                let (data, _) = try await provider.data(.userProfile(""))
-                let user = try JSONDecoder().decode(GithubUser.self, from: data)
-                XCTAssertEqual(user, GithubUser(login: "artemisia-absynthium"))
-                expectation.fulfill()
-            } catch {
-                XCTFail("Unexpected error: \(error.localizedDescription)")
-            }
-        }
-
-        wait(for: [expectation], timeout: timeout)
-    }
-
-    func testSigningFunctionWithCombineRequest() throws {
-        let signingFunction: (Github, URLRequest) async throws -> URLRequest = { _, request in
-            var mutableRequest = request
-            var url = request.url?.absoluteString ?? ""
-            url += "artemisia-absynthium"
-            mutableRequest.url = URL(string: url)
-            return mutableRequest
-        }
-        let expectation = XCTestExpectation(
-            description: "Request is modified by the signingPublisher and returns a valid user")
-
-        let provider = ArachneProvider<Github>(signingFunction: signingFunction, plugins: nil)
-        cancellable = provider.request(.userProfile(""))
-            .decode(type: GithubUser.self, decoder: JSONDecoder())
-            .sink { completion in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    XCTFail("Unexpected error: \(error.localizedDescription)")
-                }
-            } receiveValue: { user in
-                XCTAssertEqual(user, GithubUser(login: "artemisia-absynthium"))
-                expectation.fulfill()
-            }
-
-        wait(for: [expectation], timeout: timeout)
-    }
-
-    func testSigningFunctionWithAsyncRequest() throws {
+    func testSigningFunction() throws {
         let signingFunction: (Github, URLRequest) async throws -> URLRequest = { _, request in
             var mutableRequest = request
             var url = request.url?.absoluteString ?? ""
